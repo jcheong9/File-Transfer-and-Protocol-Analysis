f46d72c0946760736ed695e8b03a1d4c17aaf6aa
SmallYellowFace <jctheathlete@gmail.com>
2020-02-01 01:54:22 -0800

    running server with h file

--- FileTransferAndProtocolAnalysis/tcp_server.cpp
+++ FileTransferAndProtocolAnalysis/tcp_server.cpp
@@ -1,32 +1,4 @@
-#include "application.h"
-
-#include <windowsx.h>
-#include <stdio.h>
-#include <conio.h>
-
-#define PORT 5150
-#define DATA_BUFSIZE 8192
-
-typedef struct _SOCKET_INFORMATION {
-	BOOL RecvPosted;
-	CHAR Buffer[DATA_BUFSIZE];
-	WSABUF DataBuf;
-	SOCKET Socket;
-	DWORD BytesSEND;
-	DWORD BytesRECV;
-	_SOCKET_INFORMATION* Next;
-} SOCKET_INFORMATION, * LPSOCKET_INFORMATION;
-
-#define WM_SOCKET (WM_USER + 1)
-
-void CreateSocketInformation(SOCKET s);
-LPSOCKET_INFORMATION GetSocketInformation(SOCKET s);
-void FreeSocketInformation(SOCKET s);
-
-HWND MakeWorkerWindow(void);
-LRESULT CALLBACK tcpCallBack(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
-LPSOCKET_INFORMATION SocketInfoList;
+#include "tcp_server.h"
 
 void serverMain()
 {
---
+++ FileTransferAndProtocolAnalysis/tcp_server.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "application.h"
+
+#include <windowsx.h>
+#include <stdio.h>
+#include <conio.h>
+
+#define PORT 5150
+#define DATA_BUFSIZE 8192
+
+typedef struct _SOCKET_INFORMATION {
+	BOOL RecvPosted;
+	CHAR Buffer[DATA_BUFSIZE];
+	WSABUF DataBuf;
+	SOCKET Socket;
+	DWORD BytesSEND;
+	DWORD BytesRECV;
+	_SOCKET_INFORMATION* Next;
+} SOCKET_INFORMATION, * LPSOCKET_INFORMATION;
+
+#define WM_SOCKET (WM_USER + 1)
+void serverMain();
+void CreateSocketInformation(SOCKET s);
+LPSOCKET_INFORMATION GetSocketInformation(SOCKET s);
+void FreeSocketInformation(SOCKET s);
+
+HWND MakeWorkerWindow(void);
+LRESULT CALLBACK tcpCallBack(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+LPSOCKET_INFORMATION SocketInfoList;
