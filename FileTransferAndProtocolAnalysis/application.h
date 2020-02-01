a99d4da6fbe585175a6789c1dd1e648d99d56d61
SmallYellowFace <jctheathlete@gmail.com>
2020-02-01 01:52:06 -0800

    added server

--- FileTransferAndProtocolAnalysis/application.h
+++ FileTransferAndProtocolAnalysis/application.h
@@ -22,10 +22,10 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
-
+#include <winsock2.h>
 #include <stdio.h>
 #include <string>
-#include <Windows.h>
+#include <windows.h>
 #pragma comment(lib, "Ws2_32.lib")
 
 
